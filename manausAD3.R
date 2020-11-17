e4b12e5d0ec567f13c0598ed0a67fd965fd37036
Alexander Brandao <69222583+alexander-brandao@users.noreply.github.com>
2020-11-17 15:31:33 -0300

    Add files via upload

---
+++ manausAD3.R
@@ -0,0 +1,52 @@
+#Alexander Costa Brandao
+# Arvore de Decisao – sem pré-processamento  
+
+#a base de dados "manaus" mede a altura do rio negro em manaus ao longo do tempo. Os dados cobrem 90 anos, de janeiro de 1903 a dezembro de 1992.
+
+#comando para carregar a base de dados
+base = read.csv('manaus.csv')
+#comando para cancelar uma coluna irrelevante
+base$X = NULL
+
+#comando para instalar pacote "tidyverse'
+install.packages("tidyverse")
+                    
+library(tidyverse)
+                    
+#comando para instalar um pacote da biblioteca
+install.packages('caTools')
+                    
+library(caTools)
+                    
+View(base)
+                    
+#comando para setar dados aleatorios necessario para criar uma base de treinamento
+set.seed(1)
+                    
+#comando para dividir a base de dados com a finalidade de criar uma (base de treinamento) e uma (base de teste)
+#coloca dentro de um objeto uma funcao chamada (sample split) que indica uma coluna alvo(no caso value) para dividir 
+divisao = sample.split(base$value, SplitRatio = 0.75)
+                    
+#comando para criar a variavel base_treinamento
+base_treinamento = subset(base, divisao == TRUE)
+                    
+#comando para criar a variavel base_teste
+base_teste = subset(base, divisao == FALSE)
+                    
+library(rpart)
+classificador = rpart(formula = value ~ ., data = base_treinamento)
+                    
+library(rpart.plot)
+rpart.plot(classificador)
+                    
+previsoes = predict(classificador, newdata = base_teste[-2], type = 'class')
+matriz_confusao = table( base_teste[, 2], previsoes)
+                    
+install.packages("caret")
+install.packages("lattice")
+install.packages("ggplot2")
+library(caret)
+library(lattice)
+library(ggplot2)
+                    
+confusionMatrix(matriz_confusao)
