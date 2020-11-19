2191dbb25f5ae6c4b4b262aeb6616ab6a90f9b4f
Alexander Brandao <69222583+alexander-brandao@users.noreply.github.com>
2020-11-19 17:28:29 -0300

    Add files via upload

---
+++ manausRF1.R
@@ -0,0 +1,78 @@
+#Alexander Costa Brandao
+# RANDOM FOREST – inconsistentes + faltantes + escalonamento
+
+#a base de dados "manaus" mede a altura do rio negro em manaus ao longo do tempo. Os dados cobrem 90 anos, de janeiro de 1903 a dezembro de 1992.
+
+#comando para carregar a base de dados
+base = read.csv('manaus.csv')
+
+#comando para cancelar uma coluna irrelevante
+base$X = NULL
+
+#comando para criar varialvel com valores negativos e N/A(not avaible)
+(a = base[base$value < 0 & !is.na(base$value),  ])
+
+#comando para calcular a media sem contar os N/A
+mean(base$value[base$value > 0], na.rm = TRUE)
+
+#comando para setar a media nos valores negativos
+base$value = ifelse(base$value < 0, 1.094544, base$value)
+
+#comando para setar a media nos valores N/A
+base$value = ifelse(is.na(base$value), mean(base$value,na.rm = TRUE), base$value
+                    
+#comando para escalonar a coluna time, ja que a coluna value e a coluna alvo do algoritmo 
+base [ , 1:1] = scale(base[ , 1:1])
+                    
+#comando para instalar pacote "tidyverse'
+install.packages("tidyverse")
+                    
+library(tidyverse)
+                    
+#comando para instalar um pacote da biblioteca
+install.packages('caTools')
+                    
+library(caTools)
+                    
+View(base)
+                    
+#comando para setar dados aleatorios necessario para criar uma base de treinamento
+set.seed(1)
+                    
+#comando para dividir a base de dados com a finalidade de criar uma (base de treinamento) e uma (base de teste)
+#coloca dentro de um objeto uma funcao chamada (sample split) que indica uma coluna alvo(no caso value) para dividir 
+divisao = sample.split(base$value, SplitRatio = 0.75)
+                    
+#comando para criar a variavel base_treinamento
+base_treinamento = subset(base, divisao == TRUE)
+                    
+#comando para criar a variavel base_teste
+base_teste = subset(base, divisao == FALSE)
+
+#comando para instalar o pacote (random forest)
+install.packages('randomForest')
+library(randomForest)
+set.seed(1)
+
+
+#laço de repetiçao com 30 elementos
+for (i in 1:30) {
+  
+  # O algoritmo random forest trabalha com 2 parametros x= base de treinamento e y= classe alvo
+  
+  # escolhemos a quantidades de 'arvores para o execuçao do algoritmo (no caso 30)
+  
+  #lembrando que o classificador e um modelo de aprendizado
+  classificador = randomForest(x= base_treinamento[-2], y= base_treinamento$value, ntree = i)
+  
+  previsoes = predict(classificador, newdata = base_teste[-2], type = 'class')
+  matriz_confusao = table(base_teste[, 2], previsoes )
+  print(matriz_confusao)
+  library(caret)
+  
+  print(confusionMatrix(matriz_confusao))
+  conf_Marix <- confusionMatrix(matriz_confusao)
+  print(conf_Marix)
+  
+}
+                    
